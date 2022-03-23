42ae9379fc3b7eba89920bb5234c1f1ecaf4213b
Gifty-Arthur <giftyarthur133@gmail.com>
2022-03-23 15:22:31 +0000

    blah blah blah

--- App.js
+++ App.js
@@ -1,30 +1,27 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {NativeContainer, NavigationContainer} from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import login from './Screens/login';
 
 
 
 
-const Stack = createStackNavigator();
-import SplashScreen from './Screens/SplashScreen';
 
-1
+const Stack = createNativeStackNavigator();
+import login from './Screens/login';
+
 
 const App = () => {
   return (
     <NavigationContainer>
-    
       <Stack.Navigator>
       {/* screenOptions={{header: () => null}} */}
         {/* <Stack.Screen name= "signUp" component={signUp}/> */}
-        <Stack.Screen name= "SplashScreen" component={SplashScreen}/>
-               
-        
-
+        <Stack.Screen name= "login" component={login}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-export default SplashScreen;
+export default App;
